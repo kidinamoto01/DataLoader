e6ffe0159d69bf60466bca88ae05c3c71940e5dd
kidinamoto01 <kidinamoto@qq.com>
2017-04-19 15:00:49 +0800

    Initial commit

---
+++ src/main/scala/com/sheshou/SaveKafkaData.scala
@@ -0,0 +1,138 @@
+package com.sheshou
+
+import java.util
+
+import org.apache.spark.sql.SaveMode
+import org.apache.spark.sql.types.{StringType, StructType}
+import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.streaming.kafka.KafkaUtils
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+import java.util.Calendar
+
+import kafka.serializer.StringDecoder
+/**
+  * Created by suyu on 17-4-13.
+  * Function: save kafka data into HDFS
+  */
+object SaveKafkaData {
+
+  def main(args: Array[String]) {
+    if (args.length < 2) {
+      System.err.println(s"""
+                            |Usage: DirectKafkaWordCount <brokers> <topics>
+                            |  <brokers> is a list of one or more Kafka brokers
+                            |  <topics> is a list of one or more kafka topics to consume from
+        """.stripMargin)
+      System.exit(1)
+    }
+
+
+    val Array(brokers, topics) = args
+    println(brokers)
+    println(topics)
+
+    // Create context with 2 second batch interval
+    val sparkConf = new SparkConf().setAppName("SaveKafkaData").setMaster("local[*]")
+    //sparkConf.set("spark.hadoop.parquet.enable.summary-metadata", "true")
+    //spark.hadoop.parquet.enable.summary-metadata false
+    val  sc = new SparkContext(sparkConf)
+    val ssc = new StreamingContext(sc, Seconds(10))
+
+
+    // Create direct kafka stream with brokers and topics
+    val topicsSet = topics.split(",").toSet
+    val kafkaParams = Map[String, String]("metadata.broker.list" -> brokers,
+      "zookeeper.connect" -> "localhost:2181")
+
+    val messages = KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](
+      ssc, kafkaParams,  Set("webmiddle")).map(_._2)
+
+    val messages2 = KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](
+      ssc, kafkaParams, Set("netstds")).map(_._2)
+
+    val messages3 = KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](
+      ssc, kafkaParams, Set("windowslogin")).map(_._2)
+
+    //Get Date
+    val cal = Calendar.getInstance()
+    val date = cal.get(Calendar.DATE)
+    val Year = cal.get(Calendar.YEAR)
+    val Month1 = cal.get(Calendar.MONTH)
+    val Month = Month1+1
+    val Hour = cal.get(Calendar.HOUR_OF_DAY)
+
+    // Get the lines
+    messages.foreachRDD{ x =>
+
+      val sqlContext = new org.apache.spark.sql.SQLContext(sc)
+      // val text = sqlContext.read.json(x)
+      val text = sqlContext.read.json(x)
+
+      //get json schame
+      text.toDF().printSchema()
+
+      //save text into parquet file
+      //make sure the RDD is not empty
+      if(text.count()>0)
+      {
+
+        println("write")
+        //text.write.format("parquet").mode(SaveMode.Append).parquet("hdfs://192.168.1.21:8020/tmp/sheshou/parquet/")
+
+        text.write.format("parquet").mode(SaveMode.Append).parquet("hdfs://192.168.1.21:8020/sheshou/data/parquet/"+"webmiddle"+"/"+Year+"/"+Month+"/"+date+"/"+Hour+"/")
+      }
+
+    }
+
+    // Get the lines
+    messages2.foreachRDD{ x =>
+
+      val sqlContext = new org.apache.spark.sql.SQLContext(sc)
+      // val text = sqlContext.read.json(x)
+      val text = sqlContext.read.json(x)
+
+      //get json schame
+      text.toDF().printSchema()
+
+      //save text into parquet file
+      //make sure the RDD is not empty
+      if(text.count()>0)
+      {
+
+        println("write2")
+        //text.write.format("parquet").mode(SaveMode.Append).parquet("hdfs://192.168.1.21:8020/tmp/sheshou/parquet/")
+
+        text.write.format("parquet").mode(SaveMode.Append).parquet("hdfs://192.168.1.21:8020/sheshou/data/parquet/"+"netstds"+"/"+Year+"/"+Month+"/"+date+"/"+Hour+"/")
+      }
+
+    }
+
+
+    // Get the lines
+    messages3.foreachRDD{ x =>
+
+      val sqlContext = new org.apache.spark.sql.SQLContext(sc)
+      // val text = sqlContext.read.json(x)
+      val text = sqlContext.read.json(x)
+
+      //get json schame
+      text.toDF().printSchema()
+
+      //save text into parquet file
+      //make sure the RDD is not empty
+      if(text.count()>0)
+      {
+
+        println("write3")
+        //text.write.format("parquet").mode(SaveMode.Append).parquet("hdfs://192.168.1.21:8020/tmp/sheshou/parquet/")
+
+        text.write.format("parquet").mode(SaveMode.Append).parquet("hdfs://192.168.1.21:8020/sheshou/data/parquet/"+"windowslogin"+"/"+Year+"/"+Month+"/"+date+"/"+Hour+"/")
+      }
+
+    }
+
+    // Start the computation
+    ssc.start()
+    ssc.awaitTermination()
+  }
+}
---
+++ src/main/scala/com/sheshou/Write2Mysql.scala
@@ -0,0 +1,54 @@
+package com.sheshou
+
+import java.util.{Calendar, Properties}
+
+import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.sql.{SQLContext, SaveMode}
+
+/**
+  * Created by suyu on 17-4-18.
+  */
+object Write2Mysql {
+
+  def main(args: Array[String]) {
+
+    if (args.length < 5) {
+      System.err.println(s"""
+                            |Usage: DirectKafkaWordCount <brokers> <topics>
+                            |  <master> is a list of one or more Kafka brokers
+                            |  <path> is a list of one or more kafka topics to consume from
+                            |  <mysqlurl>
+                            |  <databasename>
+                            |  <tablename>
+        """.stripMargin)
+      System.exit(1)
+    }
+    val Array(master,path,mysqlurl,databasename,tablename) = args
+    println(master)
+    println(path)
+    println(mysqlurl)
+    println(databasename)
+    println(tablename)
+    /*
+    val logFile = "/usr/local/share/spark-2.1.0-bin-hadoop2.6/README.md" // Should be some file on your system
+    val filepath = "hdfs://192.168.1.21:8020/sheshou/data/parquet/realtime/forcebreak/2017/4/16/17"
+    val middlewarepath = "hdfs://192.168.1.21:8020/user/root/test/webmiddle/20170413/web.json"
+    val hdfspath = "hdfs://192.168.1.21:8020/user/root/test/windowslogin/20170413/windowslogin"*/
+    val filepath = "hdfs://"+master+":8020"+path
+    val conf = new SparkConf().setAppName("Offline Doc Application").setMaster("local[*]")
+    val sc = new SparkContext(conf)
+    val sqlContext = new SQLContext(sc)
+    //read json file
+    val file =sqlContext.read.parquet(filepath)//.toDF()
+
+    //MySQL connection property
+    val prop = new Properties()
+    prop.setProperty("user", "root")
+    prop.setProperty("password", "andlinks")
+
+    val dfWriter = file.write.mode("overwrite").option("driver", "com.mysql.jdbc.Driver")
+    dfWriter.jdbc("jdbc:mysql://"+mysqlurl+"/log_info", tablename, prop)
+
+  }
+
+}
